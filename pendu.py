ae634a1776e1a96f71faa6e3c282e9a2c12143c1
mbiapakj <joeltresor.mbiapaketcheckmen@edu.esiee.fr>
2022-01-25 00:20:34 +0100

    fini

--- pendu.py
+++ pendu.py
@@ -76,40 +76,50 @@ def affichage(taille: int):
 
 
 def ask_letter():
-    return input("Entrez une lettre de l'alphabet ! ").lower()
+    return input("\nEntrez une lettre de l'alphabet : ").lower()
 
 
 def error_state():
     print(HANGMANPICS[ERROR])
 
 
-def game(letter, word_to_guess):
+def game(letter, word_to_guess, hidden_word):
     global ERROR
     if letter not in word_to_guess:
-        print("Vous venez de faire une erreur !")
+        print("Vous venez de faire une erreur !\n")
         error_state()
         ERROR += 1
     else:
         list_word_to_guess = list(word_to_guess)
-        for index, value in enumerate(list_word_to_guess):
-            if value != letter:
-                list_word_to_guess[index] = "_"
-        hidden_word = " ".join(list_word_to_guess)
-        print(hidden_word)
-        if ERROR > 1:
-            error_state()
+        list_hidden_word = hidden_word.split(" ")
+
+        if letter not in list_hidden_word:
+            for index, value in enumerate(list_word_to_guess):
+                if value == letter:
+                    list_hidden_word[index] = letter
+
+            hidden_word = " ".join(list_hidden_word)
+            print(hidden_word, "\n")
+            if ERROR > 1:
+                error_state()
+        else:
+            print("Vous avez déjà entrez cette lettre, choisissez s'en une autre !\n")
+    return hidden_word
 
 
 def main():
     word_data = read_file(WORDFILE)
     word = random_word(word_data)
     hidden_word = affichage(len(word))
-    print(word, hidden_word)
+    print(hidden_word)
     while 1:
-        game(ask_letter(), word)
+        hidden_word = game(ask_letter(), word, hidden_word)
         if ERROR == len(HANGMANPICS):
             print("---- GAME OVER ----")
             print(f"Le bon mot était : {word}")
+            return
+        if "_" not in hidden_word:
+            print("---- Victoir du joueur français ----")
             return
 
 
